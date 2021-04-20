44b0be600e6dbc27848396a76bcf7e06e1afb2b4
mohi16 <75882258+mohi16@users.noreply.github.com>
2021-04-20 13:20:28 +0200

    init

---
+++ Frontend/app.js
@@ -0,0 +1,21 @@
+"use strict";
+/*
+   Achtung - wichtige Hinweise:
+   -----------------------------------------------------------------------------
+   1) Sollte VSC jQuery nicht kennen, dann müssen die Typen erst importiert werden
+      Führen Sie dazu Folgendes im Terminal von VSC aus:
+         npm install --save @types/jquery
+   2) Fehlermeldung beim Ausführen von Ajax-Requests:
+      "Quellübergreifende (Cross-Origin) Anfrage blockiert: Die Gleiche-Quelle-Regel verbietet das Lesen der externen Ressource ..."
+      --> das passiert wenn Client und Server von unterschiedlichen Quellen kommen
+          (z.B. Client: http://localhost:3000/...
+                Server: http://localhost:80/... )
+      --> dann muss für den Server angegeben werden, dass er das Ergebnis ausliefern darf
+      --> Erstellen einer .htaccess - Datei im Verzeichnis der anzusprechenden PHP-Datei mit folgendem Inhalt:
+          Header set Access-Control-Allow-Origin "*"
+*/
+// Settings:
+var restServer = "http://localhost:80/21WS/Pr/Backend/serviceHandler.php";
+$.getJSON(restServer, { 'method': 'queryPersons' }, function (data) {
+    $('#mainpart').text(JSON.stringify(data));
+});
